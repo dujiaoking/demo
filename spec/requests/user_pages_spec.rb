7cead8b472b3bf5d9ef8bfca7c7479aea7999b49
dujiaoking <dujiaoking@163.com>
2014-01-19 21:31:50 -0800

    Finish user edit ,update,index ,and destroy actions

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,12 +1,15 @@
 require 'spec_helper'
+
 describe "User pages" do
 subject { page }
+
 describe "profile page" do
 let(:user) { FactoryGirl.create(:user) }
 before { visit user_path(user) }
 it { should have_content(user.name) }
 it { should have_title(user.name) }
 end
+
 describe "signup page" do
 before { visit signup_path }
 it { should have_content('Sign up') }
@@ -33,4 +36,29 @@ expect { click_button submit }.to change(User, :count).by(1)
 end
 end
 end
+
+describe "edit" do
+let(:user) { FactoryGirl.create(:user) }
+before do
+sign_in user
+visit edit_user_path(user)
 end
+
+describe "edit" do
+let(:user) { FactoryGirl.create(:user) }
+before { visit edit_user_path(user) }
+describe "page" do
+it { should have_content("Update your profile") }
+it { should have_title("Edit user") }
+it { should have_link('change', href: 'http://gravatar.com/emails') }
+end
+describe "with invalid information" do
+before { click_button "Save changes" }
+it { should have_content('error') }
+end
+
+
+
+end
+end
+
